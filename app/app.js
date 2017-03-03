a83fddc89aeffd057391fb9ace100dfad9f3eff1
KJ <kj1352>
2017-03-03 21:19:40 +0530

    Initail Commit

--- app/app.js
+++ app/app.js
@@ -2,13 +2,14 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
+  'ui.bootstrap',
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.view3',
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/stories'});
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,23 @@
-'use strict';
-
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+    .config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/stories', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$http',function($scope, $http) {
+
+
+  $http({
+        method : "GET",
+        url : "http://bane-was-bored.herokuapp.com/posts/"
+    }).then(function mySucces(response) {
+        console.log(response.data);
+        $scope.results=response.data;
+      }, function myError(response) {
+        console.log(response);
+    });
 
 }]);
